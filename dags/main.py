d3ac6252521560ef53a8f630697a004c81a8fa27
dylanlo8 <lojingjiedylan@gmail.com>
2024-04-01 01:20:11 +0800

    finalised DAG

--- dags/main.py
+++ dags/main.py
@@ -11,6 +11,8 @@ import asyncio
 import math
 import re
 import numpy as np
+import pandas_gbq
+
 
 client = bigquery.Client()
 
@@ -20,84 +22,61 @@ default_args = {
     "email": ["lojingjiedylan@gmail.com"],
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 3,
-    "retry_delay": timedelta(minutes=10)
+    "retries": 2,
+    "retry_delay": timedelta(minutes=1)
 }
 
+# Variables
+datagov_name_and_id_buildings = ("buildings", "d_17f5382f26140b1fdae0ba2ef6239d2f")
+datagov_name_and_id_median_rent = ("median_rent", "d_23000a00c52996c55106084ed0339566")
+datagov_name_and_id_schools = ("schools", "d_688b934f82c1059ed0a6993d2a829089")
+datagov_name_and_id_trans1 = ("transactions1_data", "d_ebc5ab87086db484f88045b47411ebc5")
+datagov_name_and_id_trans2 = ("transactions2_data", "d_43f493c6c50d54243cc1eab0df142d6a")
+datagov_name_and_id_trans3 = ("transactions3_data", "d_2d5ff9ea31397b66239f245f57751537")
+datagov_name_and_id_trans4 = ("transactions4_data", "d_ea9ed51da2787afaf8e51f827c304208")
+datagov_name_and_id_trans5 = ("transactions5_data", "d_8b84c4ee58e3cfc0ece0d773c8ca6abc")
+
+town_mapping = {
+    'AMK': 'ANG MO KIO', 'BB': 'BUKIT BATOK', 'BD': 'BEDOK', 'BH': 'BISHAN', 'BM': 'BUKIT MERAH',
+    'BP': 'BUKIT PANJANG', 'BT': 'BUKIT TIMAH', 'CCK': 'CHOA CHU KANG', 'CL': 'CLEMENTI', 'CT': 'CENTRAL AREA',
+    'GL': 'GEYLANG', 'HG': 'HOUGANG', 'JE': 'JURONG EAST', 'JW': 'JURONG WEST', 'KWN': 'KALLANG/WHAMPOA',
+    'MP': 'MARINE PARADE', 'PG': 'PUNGGOL', 'PRC': 'PASIR RIS', 'QT': 'QUEENSTOWN', 'SB': 'SEMBAWANG',
+    'SGN': 'SERANGOON', 'SK': 'SENGKANG', 'TAP': 'TAMPINES', 'TG': 'TENGAH', 'TP' : 'TOA PAYOH' ,
+    'WL' : 'WOODLANDS' , 'YS' : 'YISHUN'
+}
+
+project_id = 'is3107-415212'
+
 # HELPER FUNCTIONS
-async def extract_raw_data_async():
+async def extract_raw_data_async(resource_id):
     data_gov_base_url = "https://data.gov.sg/api/action/datastore_search?resource_id="
-    headers = {"User-Agent": "Mozilla/5.0"}
+    try:
+        base_url = f"{data_gov_base_url}{resource_id}"
+        check = requests.get(base_url)
+        record_count = check.json()["result"]["total"]
+    except KeyError:
+        print(f"Invalid result response, check the url: {base_url}")
+        return None
+    except Exception as e:
+        print(f"Failed to access the API at url: {base_url}")
+        print(e)
+        return None
 
-    datagov_name_and_ids = [
-        ("buildings", "d_17f5382f26140b1fdae0ba2ef6239d2f"),
-        ("transactions1_data", "d_ebc5ab87086db484f88045b47411ebc5"),
-        ("transactions2_data", "d_43f493c6c50d54243cc1eab0df142d6a"),
-        ("transactions3_data", "d_2d5ff9ea31397b66239f245f57751537"),
-        ("transactions4_data", "d_ea9ed51da2787afaf8e51f827c304208"),
-        ("transactions5_data", "d_8b84c4ee58e3cfc0ece0d773c8ca6abc"),
-        ("median_rent", "d_23000a00c52996c55106084ed0339566"),
-        ("schools", "d_688b934f82c1059ed0a6993d2a829089")
-    ]
-    
-    async def datagovsg_id_to_csv_async(resource_id, filename, row_max_limit=5000, iterations=10):
-        try:
-            base_url = f"{data_gov_base_url}{resource_id}"
-            check = requests.get(base_url, headers=headers)
-            record_count = check.json()["result"]["total"]
-        except KeyError:
-            print(f"Invalid result response, check the url: {base_url}")
-            return None
-        except Exception as e:
-            print(f"Failed to access the API at url: {base_url}")
-            print(e)
-            return None
-        
-        async def fetch(s, url):
-            async with s.get(url) as result:
-                if result.status != 200:
-                    result.raise_for_status()
-                output = await result.json()
-                try:
-                    output = output["result"]["records"]
-                    return output
-                except:
-                    print(f"Error downloading at at url: {url}")
-                    print(output)
+    async with aiohttp.ClientSession() as session:
+        async def fetch(url):
+            async with session.get(url) as response:
+                if response.status != 200:
+                    response.raise_for_status()
+                return await response.json()
 
-        async def fetch_all(s, urls):
-            tasks = []
-            for url in urls:
-                task = asyncio.create_task(fetch(s, url))
-                tasks.append(task)
-            completed = await asyncio.gather(*tasks)
-            result = []
-            for complete in completed:
-                result.extend(complete)
-            return result
-
-        rows_per_iteration = min(math.ceil(record_count / iterations), row_max_limit)
+        rows_per_iteration = min(math.ceil(record_count / 10), 5000)
         num_calls = math.ceil(record_count / rows_per_iteration)
 
-        urls = [f"{base_url}&offset={i * rows_per_iteration}&limit={rows_per_iteration}" for i in range(0, num_calls)]
-        async with aiohttp.ClientSession(headers=headers) as s:
-            downloaded_records = await fetch_all(s, urls)
-        
-        if len(downloaded_records) != record_count:
-            print(f"Dataset {resource_id} ({filename}) did not download completely.")
-            print(f"Expected row counts: {record_count}")
-            print(f"Downloaded row counts: {len(downloaded_records)}")
+        urls = [f"{base_url}&offset={i * rows_per_iteration}&limit={rows_per_iteration}" for i in range(num_calls)]
+        tasks = [fetch(url) for url in urls]
+        downloaded_records = await asyncio.gather(*tasks)
 
-        pd.DataFrame.from_records(downloaded_records).to_csv(f"{filename}.csv", index=False)
-        return 
-        
-    tasks = []
-    for filename, dataset_id in datagov_name_and_ids:
-        task = asyncio.create_task(datagovsg_id_to_csv_async(dataset_id, filename))
-        tasks.append(task)
-    await asyncio.gather(*tasks)
-
-    return [filename for filename, _ in datagov_name_and_ids]
+    return [record for call in downloaded_records for record in call['result']['records']]
 
 def onemap_api_postal(data):
     async def fetch(session, blk_no, street):
@@ -125,62 +104,113 @@ def onemap_api_postal(data):
 
     return asyncio.run(main())
 
-town_mapping = {
-        'AMK': 'ANG MO KIO', 'BB': 'BUKIT BATOK', 'BD': 'BEDOK', 'BH': 'BISHAN', 'BM': 'BUKIT MERAH',
-        'BP': 'BUKIT PANJANG', 'BT': 'BUKIT TIMAH', 'CCK': 'CHOA CHU KANG', 'CL': 'CLEMENTI', 'CT': 'CENTRAL AREA',
-        'GL': 'GEYLANG', 'HG': 'HOUGANG', 'JE': 'JURONG EAST', 'JW': 'JURONG WEST', 'KWN': 'KALLANG/WHAMPOA',
-        'MP': 'MARINE PARADE', 'PG': 'PUNGGOL', 'PRC': 'PASIR RIS', 'QT': 'QUEENSTOWN', 'SB': 'SEMBAWANG',
-        'SGN': 'SERANGOON', 'SK': 'SENGKANG', 'TAP': 'TAMPINES', 'TG': 'TENGAH', 'TP' : 'TOA PAYOH' ,
-        'WL' : 'WOODLANDS' , 'YS' : 'YISHUN'
-}
+def onemap_api_mrt_stations():
+    # Making simple request query to get totalNumPages
+    search_val = "MRT Station"
+    url = f"https://www.onemap.gov.sg/api/common/elastic/search?searchVal={search_val}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
+    response = requests.request("GET", url)
+    num_pages = response.json()['totalNumPages']
 
+    # Async Fetch function
+    async def fetch(session, page_num):
+        url = f"https://www.onemap.gov.sg/api/common/elastic/search?searchVal={search_val}&returnGeom=Y&getAddrDetails=Y&pageNum={page_num}"
+        async with session.get(url) as response:
+            try:
+                query = await response.json()
+                query = query['results']
+                return query
+            except:
+                return None
+    
+    # Async main function to fetch all records
+    async def main():
+        tasks = []
+        page_num = 1 # Starts with page 1 of query
+        async with aiohttp.ClientSession() as session:
+            while page_num <= num_pages:
+                task = asyncio.create_task(fetch(session, page_num))
+                tasks.append(task)
+                page_num += 1
+            results = await asyncio.gather(*tasks)
+        return results
+
+    return asyncio.run(main())
 
 # DAG
-@dag(dag_id='is3107_groupproject', default_args=default_args, schedule=None, catchup=False, tags=['is3107'])
-def assignment3_dag():
+@dag(dag_id='is3107', default_args=default_args, schedule=None, catchup=False, tags=['is3107'])
+def dag():
     # Extract Tasks
     @task
-    def extract_raw_data():
-        return asyncio.run(extract_raw_data_async())
-
-    # Read Data Tasks
+    def extract_trans1():
+        records = asyncio.run(extract_raw_data_async(datagov_name_and_id_trans1[1]))
+        df = pd.DataFrame(records)
+        return df
+    
     @task
-    def read_buildings():
-        return pd.read_csv('buildings.csv', index_col = 0).reset_index(drop = True)
-
-    @task
-    def read_schools():
-        return pd.read_csv('schools.csv')
-
-    @task
-    def read_rent():
-        return pd.read_csv('median_rent.csv')
+    def extract_trans2():
+        records = asyncio.run(extract_raw_data_async(datagov_name_and_id_trans2[1]))
+        df = pd.DataFrame(records)
+        return df
 
     @task
-    def read_transactions():
-        trans1 = pd.read_csv('transactions1_data.csv', index_col=0).reset_index(drop = True)
-        trans2 = pd.read_csv('transactions2_data.csv', index_col=0).reset_index(drop = True)
-        trans3 = pd.read_csv('transactions3_data.csv', index_col=0).reset_index(drop = True)
-        trans4 = pd.read_csv('transactions4_data.csv', index_col=0).reset_index(drop = True)
-        trans5 = pd.read_csv('transactions5_data.csv', index_col=0).reset_index(drop = True)
+    def extract_trans3():
+        records = asyncio.run(extract_raw_data_async(datagov_name_and_id_trans3[1]))
+        df = pd.DataFrame(records)
+        return df    
 
-        merged_transactions = pd.concat([trans1, trans2, trans3, trans4, trans5], ignore_index=True).reset_index()
-        merged_transactions = merged_transactions.rename({'index': 'transaction_id'}, axis = 1)
-        return merged_transactions
+    @task
+    def extract_trans4():
+        records = asyncio.run(extract_raw_data_async(datagov_name_and_id_trans4[1]))
+        df = pd.DataFrame(records)
+        return df
+
+    @task
+    def extract_trans5():
+        records = asyncio.run(extract_raw_data_async(datagov_name_and_id_trans5[1]))
+        df = pd.DataFrame(records)
+        return df
+    
+    @task
+    def extract_schools():
+        records = asyncio.run(extract_raw_data_async(datagov_name_and_id_schools[1]))
+        df = pd.DataFrame(records)
+        df = df.drop(df.columns[0], axis=1)
+        return df
+    
+    @task
+    def extract_buildings():
+        records = asyncio.run(extract_raw_data_async(datagov_name_and_id_buildings[1]))
+        df = pd.DataFrame(records)
+        df = df.drop(df.columns[0], axis=1)
+        return df
+    
+    @task
+    def extract_rent():
+        records = asyncio.run(extract_raw_data_async(datagov_name_and_id_median_rent[1]))
+        df = pd.DataFrame(records)
+        df = df.drop(df.columns[0], axis=1)
+        return df
+    
+   
 
     # Transform Tasks
     # Buildings Entity
     @task
     def transform_buildings(buildings):
-        # Run OneMap API to get postal code
+        # Run OneMap API to get postal code 
         buildings['postal_code'] = onemap_api_postal(buildings)
 
         # Convert to the correct formatting for Town
-        buildings['bldg_contract_town'] = buildings['bldg_contract_town'].map(town_mapping)
+        buildings['town'] = buildings['bldg_contract_town'].map(town_mapping)
+        buildings = buildings.drop('bldg_contract_town', axis = 1)
 
+        return buildings
+    
+    @task
+    def drop_building_columns(buildings):
         # Remove irrelevant columns
         retained_cols = ['postal_code', 'blk_no', 'street', 'max_floor_lvl', 'year_completed', 
-                         'multistorey_carpark', 'precinct_pavilion', 'market_hawker']
+                        'multistorey_carpark', 'precinct_pavilion', 'market_hawker']
         
         buildings = buildings[retained_cols]
         return buildings
@@ -255,6 +285,12 @@ def assignment3_dag():
 
     # Transactions entity
     @task
+    def merge_transactions(list_trans):
+        merged_transactions = pd.concat(list_trans, ignore_index=True).reset_index()
+        merged_transactions = merged_transactions.rename({'index': 'transaction_id'}, axis = 1)
+        return merged_transactions
+    
+    @task
     def transform_transactions(transactions):
         # Drop the remaining_lease column
         transactions = transactions.drop(['remaining_lease'], axis = 1)
@@ -277,39 +313,7 @@ def assignment3_dag():
     
     # MRT Stations Entity
     @task
-    def create_mrt_stations():
-        def onemap_api_mrt_stations():
-            # Making simple request query to get totalNumPages
-            search_val = "MRT Station"
-            url = f"https://www.onemap.gov.sg/api/common/elastic/search?searchVal={search_val}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
-            response = requests.request("GET", url)
-            num_pages = response.json()['totalNumPages']
-
-            # Async Fetch function
-            async def fetch(session, page_num):
-                url = f"https://www.onemap.gov.sg/api/common/elastic/search?searchVal={search_val}&returnGeom=Y&getAddrDetails=Y&pageNum={page_num}"
-                async with session.get(url) as response:
-                    try:
-                        query = await response.json()
-                        query = query['results']
-                        return query
-                    except:
-                        return None
-            
-            # Async main function to fetch all records
-            async def main():
-                tasks = []
-                page_num = 1 # Starts with page 1 of query
-                async with aiohttp.ClientSession() as session:
-                    while page_num <= num_pages:
-                        task = asyncio.create_task(fetch(session, page_num))
-                        tasks.append(task)
-                        page_num += 1
-                    results = await asyncio.gather(*tasks)
-                return results
-
-            return asyncio.run(main())
-    
+    def create_mrt_stations():    
         mrt_stations_query = onemap_api_mrt_stations()
 
         # Flatten nested list and filter get unique MRT Station names with (Station Code)
@@ -319,26 +323,76 @@ def assignment3_dag():
         stations = pd.DataFrame(stations, columns = ['station_name', 'postal_code']).drop_duplicates(subset = ['postal_code'])
         return stations
     
+
+    # Load Functions
     @task
-    def test_load(data):
-        i = 0
-        for df in data:
-            df.to_csv(f"{i}.csv")
-            i += 1
-        return
+    def load_transactions(transactions):
+        pandas_gbq.to_gbq(transactions, 'hdbprices.transactions', project_id = project_id)
 
+    @task
+    def load_mrt_stations(stations):
+        pandas_gbq.to_gbq(stations, 'hdbprices.stations', project_id = project_id)
+
+    @task
+    def load_flat_type(flat_type):
+        pandas_gbq.to_gbq(flat_type, 'hdbprices.flat_type', project_id = project_id)
+
+    @task
+    def load_town(town):
+        pandas_gbq.to_gbq(town, 'hdbprices.town', project_id = project_id)
+    
+    @task
+    def load_flats_sold(flats_sold):
+        pandas_gbq.to_gbq(flats_sold, 'hdbprices.flats_sold', project_id = project_id)
+    
+    @task
+    def load_buildings(buildings):
+        pandas_gbq.to_gbq(buildings, 'hdbprices.buildings', project_id = project_id)
+    
+    @task
+    def load_rent(rent):
+        pandas_gbq.to_gbq(rent, 'hdbprices.rent', project_id = project_id)
+    
+    @task
+    def load_schools(schools):
+        pandas_gbq.to_gbq(schools, 'hdbprices.schools', project_id = project_id)
+
+
+    
     # Task dependencies
-    extract_raw_data()
-    #buildings = transform_buildings(read_buildings())
-    #flats_sold = create_flats_sold(buildings)
-    #town = create_town(town_mapping)
-    #rent = transform_median_rent(read_rent())
-    #schools = transform_schools(read_schools())
-    #transactions = transform_transactions(read_transactions())
-    #flat_type = create_flat_type(transactions)
-    #mrt_stations = create_mrt_stations()
+    buildings = extract_buildings()
+    buildings = transform_buildings(buildings)
+    flats_sold = create_flats_sold(buildings)
+    buildings = drop_building_columns(buildings)
 
-    #test_load([buildings, flats_sold, town, rent, schools, transactions, flat_type, mrt_stations])
+    rent = extract_rent()
+    rent = transform_median_rent(rent)
+
+    schools = extract_schools()
+    schools = transform_schools(schools)
+
+    # trans1 = extract_trans1()
+    # trans2 = extract_trans2()
+    # trans3 = extract_trans3()
+    # trans4 = extract_trans4()
+    trans5 = extract_trans5()
+
+    transactions = trans5
+    transactions = transform_transactions(transactions)
+    flat_type = create_flat_type(transactions)
+    town = create_town(town_mapping)
+    mrt_stations = create_mrt_stations()
+
+    # Load Functions
+    load_flats_sold(flats_sold)
+    load_town(town)
+    load_buildings(buildings)
+    load_schools(schools)
+    load_transactions(transactions)
+    load_mrt_stations(mrt_stations)
+    load_flat_type(flat_type)
+    load_rent(rent)
+
     return
 
-assignment3_etl = assignment3_dag()
+is3107_etl = dag()
